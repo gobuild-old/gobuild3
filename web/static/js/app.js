c00c6cd68bb04f4d2a1aeaf50a91b455476eb19c
codeskyblue <codeskyblue@gmail.com>
2014-10-08 16:52:33 +0800

    close #4

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -7,4 +7,18 @@ $(function(){
 	$('[data-toggle=tooltip]').mouseover(function() {
 		$(this).tooltip('show');
 	})
+
+
+	if (!String.prototype.format) {
+	  String.prototype.format = function() {
+		var args;
+		args = arguments;
+		if (args.length === 1 && args[0] !== null && typeof args[0] === 'object') {
+		  args = args[0];
+		}
+		return this.replace(/{([^}]*)}/g, function(match, key) {
+		  return (typeof args[key] !== "undefined" ? args[key] : match);
+		});
+	  };
+	};
 });
